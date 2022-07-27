76485c517258bf60fe8ebbc2fa3ab4a0a96a3e2a
Alex Kong <akong@y-intercept.net>
2022-07-27 12:25:11 +0800

    better memory management

--- pac.c
+++ pac.c
@@ -20,31 +20,31 @@ void* handle_sound_name_queue() {
     list_queue_items();
     char* sound_name = peek();
     if (sound_name == NULL) {
-        fprintf(stderr, "queue is already empty\n");
+        fprintf(stderr, "queue is already empty/memory allocation failed\n");
     }
-    if (strnlen(sound_name, PATH_MAX) >= PATH_MAX / 2) {
+    if (strnlen(sound_name, NAME_MAX) >= NAME_MAX) {
         fprintf(stderr, "sound_name too long\n");
     }
-    char sound_path[PATH_MAX], sound_realpath[PATH_MAX];
-    strcpy(sound_path, sound_repository_path);
+    char sound_path[PATH_MAX] = "", sound_realpath[PATH_MAX];
+    strcat(sound_path, sound_repository_path);
     strcat(sound_path, sound_name);
+    free(sound_name);
     realpath(sound_path, sound_realpath);
     if (sound_realpath == NULL) {
         fprintf(stderr, "sound_realpath == NULL\n");
     }
-    printf("in play_sound(), sound_name is %s; sound_realpath is %s\n", sound_name, sound_realpath);
     
     // https://stackoverflow.com/questions/5460421/how-do-you-write-a-c-program-to-execute-another-program
     pid_t pid = fork();
     if (pid == 0) { /* child process */
-        char* argv[]={"mpg123", "-o", "alsa:hw:1,0", sound_realpath, NULL};
+        //char* argv[]={"mpg123", "-o", "alsa:hw:1,0", sound_realpath, NULL};
+        char* argv[]={"mpg123", sound_realpath, NULL};
         execv("/usr/bin/mpg123",argv); // this call only returns if an error occurs!
-     //   return 4;
-    }
-    else { /* pid!=0; parent process */
+        exit(1);
+    } else { /* pid!=0; parent process */
         waitpid(pid, 0, 0); /* wait for child to exit */
         dequeue();
-        free(sound_name);
+        
         if (get_queue_size() > 0) {
             goto start_label;
         }
@@ -57,42 +57,36 @@ int index_page(void *p, onion_request *req, onion_response *res){
     if (notification_type == NULL) {
         return onion_shortcut_response("Parameter notification_type is missing", HTTP_BAD_REQUEST, req, res);
     }
-    if (strcmp(notification_type, "custom") == 0) {
-        if (sound_name == NULL || strnlen(sound_name, NAME_MAX) > NAME_MAX) {
-            return onion_shortcut_response(
-                "sound_name invalid (NULL or too long)", HTTP_BAD_REQUEST, req, res
-            );    
+    if (strcmp(notification_type, "custom") == 0 || strcmp(notification_type, "chiming") == 0) {
+        char custom_sound_name[NAME_MAX + 32] = "";
+        if (strcmp(notification_type, "chiming") == 0) {
+            strcat(custom_sound_name, "cuckoo-clock-sound-0727.mp3");
+        } else {
+            if (sound_name == NULL || strnlen(sound_name, NAME_MAX) > NAME_MAX) {
+                return onion_shortcut_response(
+                    "sound_name invalid (NULL or too long)", HTTP_BAD_REQUEST, req, res
+                );
+            }
+            strcat(custom_sound_name, "custom-event/");
+            strcat(custom_sound_name, sound_name);
         }
-        char custom_sound_name[NAME_MAX + 32] = "custom-event/";
-        strcat(custom_sound_name, sound_name);
         if (enqueue(custom_sound_name)) {
             list_queue_items();
         } else {
-            fprintf(stderr, "queue is full, new sound discarded.\n");
+            return onion_shortcut_response(
+                "queue is full/server has not enough free memory, new sound discarded.\n", HTTP_BAD_REQUEST, req, res
+            );
         }
         if (get_queue_size() == 1) {
             pthread_t my_thread;
             pthread_create(&my_thread, NULL, handle_sound_name_queue, NULL); // no parentheses here 
         }
-        return onion_shortcut_response(
-            "OK, pushed to sound_stack, notification_type == custom", HTTP_OK, req, res
+        onion_response_printf(
+            res, "OK, sound [%s] added to sound_queue, notification_type == [%s]", custom_sound_name, notification_type
         );
-    } 
-    else if (strcmp(notification_type, "chiming") == 0) {
-        if (enqueue("cuckoo-clock-sound-0727.mp3")) {
-            list_queue_items();
-        } else {
-            fprintf(stderr, "queue is full, new sound discarded.\n");
-        }
-        if (get_queue_size() == 1) {
-            pthread_t my_thread;
-            pthread_create(&my_thread, NULL, handle_sound_name_queue, NULL); // no parentheses here 
-        }
-        return onion_shortcut_response("OK! (notification_type == chiming)", HTTP_OK, req, res);
+	    return OCS_PROCESSED;
     }
-    else {
-        return onion_shortcut_response("notification_type's value is invalid", HTTP_BAD_REQUEST, req, res);
-    }
+    onion_response_printf(res, "notification_type's value [%s] is invalid\n", notification_type);
 	return OCS_PROCESSED;
 }
 
@@ -106,8 +100,8 @@ static void shutdown_server(int _){
 
 
 int main(int argc, char **argv){
-    if (argc != 2) {
-        fprintf(stderr, "sound_repository not specified");
+    if (argc != 3) {
+        fprintf(stderr, "usage: [sound_repository] [port]");
         return 1;
     } else if (strnlen(argv[1], PATH_MAX) >= PATH_MAX / 2) {
         fprintf(stderr, "sound_repository too long");
@@ -116,21 +110,20 @@ int main(int argc, char **argv){
     
     sound_repository_path = argv[1];
     
-
 	signal(SIGINT,shutdown_server);
 	signal(SIGTERM,shutdown_server);
 
 	ONION_VERSION_IS_COMPATIBLE_OR_ABORT();
     initialize_queue();
-	o=onion_new(O_POOL);
+	o=onion_new(O_THREADED);
 	onion_set_timeout(o, 5000);
 	onion_set_hostname(o,"0.0.0.0");
-	onion_set_port(o, "9527"); 	
+	onion_set_port(o, argv[2]); 	
 	onion_url *urls=onion_root_url(o);
-
 	onion_url_add(urls, "", index_page);
 
 	onion_listen(o);
 	onion_free(o);
+    finalize_queue();
 	return 0;
 }
--- queue.c
+++ queue.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <sys/mman.h> /* for mmap    */
 
-#define MAX_SOUND_QUEUE_SIZE 8
-char** sound_queue;
+#define MAX_SOUND_QUEUE_SIZE 64
+char** sound_queue = NULL;
 int* front_ptr = NULL;
 int* rear_ptr = NULL;
 
@@ -17,11 +17,17 @@ int initialize_queue() {
 }
 
 int get_queue_size() {
+  if (sound_queue == NULL || front_ptr == NULL || rear_ptr == NULL) {
+    return 0;
+  }
   return (MAX_SOUND_QUEUE_SIZE + *rear_ptr - *front_ptr) % MAX_SOUND_QUEUE_SIZE;
 }
 
 void list_queue_items() {
-    printf("Current sound_queue (front_ptr: %d, rear_prt: %d, size: %d):\n", *front_ptr, *rear_ptr, get_queue_size());
+    printf(
+      "Current sound_queue (front_ptr: %d, rear_prt: %d, size: %d, MAX_SOUND_QUEUE_SIZE: %d):\n",
+      *front_ptr, *rear_ptr, get_queue_size(), MAX_SOUND_QUEUE_SIZE - 1
+    );
     for (int i = *front_ptr; i < *front_ptr + get_queue_size(); ++i) {
         printf("%d: %s\n", i, sound_queue[i % MAX_SOUND_QUEUE_SIZE]);
     }
@@ -30,8 +36,16 @@ void list_queue_items() {
 bool enqueue(const char* sound_name) {
   if (get_queue_size() >= MAX_SOUND_QUEUE_SIZE - 1) {
     return false;
+    /*
+    Per current design, the available slot of the queue is MAX_SOUND_QUEUE_SIZE - 1
+    The reason is that front_ptr == rear_ptr could be ambiguous--it can mean either queue is empty or full.
+    To aovid this ambiguity, we define front_ptr == rear_ptr as empty and front_ptr + 1 == rear_ptr as full
+    */
   }
   sound_queue[*rear_ptr] = malloc((strlen(sound_name) + 1) * sizeof(char));
+  if (sound_queue[*rear_ptr] == NULL) {
+    return false;
+  }
   strcpy(sound_queue[*rear_ptr], sound_name);
   ++(*rear_ptr);
   *rear_ptr %= MAX_SOUND_QUEUE_SIZE;
@@ -40,15 +54,18 @@ bool enqueue(const char* sound_name) {
 }
 
 /**
- * Caller needs to free() the char pointer
+ * Return a pointer to sound_name at front_ptr or NULL in case of empty queue or memory allocation failure.
+ * Caller needs to free() the char pointer after use.
 */
 char* peek() {
   if (get_queue_size() <= 0) {
     return NULL;
   }
   char* item = malloc((strlen(sound_queue[*front_ptr]) + 1) * sizeof(char));
+  if (item == NULL) {
+    return NULL;
+  }
   strcpy(item, sound_queue[*front_ptr]);
-  
   return item;
 }
 
@@ -59,4 +76,22 @@ void dequeue() {
   free(sound_queue[*front_ptr]);
   ++(*front_ptr);
   (*front_ptr) %= MAX_SOUND_QUEUE_SIZE;
+}
+
+void finalize_queue() {
+  while(get_queue_size() > 0) {
+    dequeue();
+  }
+  if (sound_queue != NULL) {
+    free(sound_queue);
+  }
+  if (front_ptr != NULL) {
+    free(front_ptr);
+  }
+  if (rear_ptr != NULL) {
+    free(rear_ptr);
+  }
+  sound_queue = NULL;
+  front_ptr = NULL;
+  rear_ptr = NULL;
 }
